ca2d40278b347649f97220ae23b691168264a5ba
DeibsonWagner636 <deibson64@hotmail.com>
2025-07-28 16:38:06 -0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // A URL da API vem do .env
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/docs";
 
 export const getContatos = () =>
   axios.get(`${API_URL}/contatos`).then(res => res.data);
